0fbf79e4c7299526d4f63b7291e29eded8125b5a
PhooMagoo <36277252+PhooMagoo@users.noreply.github.com>
2021-05-24 22:16:58 -0500

    Add files via upload

---
+++ Mr. Manager/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Try
+
+
+
+        Catch ex As Exception
+            MessageBox.Show("An error occurred.")
+        End Try
+
+    End Sub
+
+    Private Sub tbGameRelease_TextChanged(sender As Object, e As EventArgs) Handles tbGameRelease.TextChanged
+
+        Try
+
+
+
+        Catch ex As Exception
+            MessageBox.Show("An error occurred.")
+        End Try
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Try
+
+
+
+        Catch ex As Exception
+            MessageBox.Show("An error occurred.")
+        End Try
+
+    End Sub
+
+End Class
